254e9d0225ffdac861dd1ea2f6d47f36c612a468
JAMES MOON <SENDJMOON@GMAIL.COM>
2018-09-05 15:45:40 -0700

    Create unit testing for rendering components.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import Landing from './landing';
+import Home from './home';
+import About from './about';
+import Portfolio from './portfolio';
+import Contact from './contact';
+import Nav from './nav';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the Landing component', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Landing />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the Home component', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Home />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the About component', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<About />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the Portfolio component', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Portfolio />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the Contact component', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Contact />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the Nav component', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Nav />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
